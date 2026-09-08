0ebd0af08d82aaa8c4de48fb2abbc07e806065ae
a_c <peacemakerallones@gmail.com>
2026-09-08 22:34:15 +0800

    add test place order register while checkout

---
+++ tests/test_place_order_register_while_checkout.py
@@ -0,0 +1,70 @@
+import pytest
+from playwright.sync_api import Page
+from pages.navbar_page import NavbarPage
+from pages.products_page import AddProductsPage
+from pages.register_user_page import RegistrationPage
+
+
+def test_place_order_register_while_checkout(page: Page, setup_browser):
+    login_user_page = setup_browser
+
+    navbar_page = NavbarPage(page)
+    products_page = AddProductsPage(page)
+    register_user_page = RegistrationPage(page)
+
+    products_page.add_first_product_to_cart()
+    products_page.view_cart_page()
+    products_page.verify_view_cart_page()
+    products_page.proceed_to_checkout()
+    products_page.register_login()
+
+    register_user_page.signup("junuel", "junueljon125@gmail.com")
+
+    register_user_page.male_radio.check()
+
+    register_user_page.enter_password("password123")
+
+    register_user_page.select_day("2", "10", "2000")
+
+    register_user_page.select_newsletter()
+    register_user_page.select_special_offers()
+
+    register_user_page.enter_address_info(
+        first_name="junuel",
+        last_name="dizon",
+        company="ABC Company",
+        address1="123 Main St",
+        address2="Apt 4B",
+        country="United States",
+        city="New York",
+        state="NY",
+        zipcode="10001",
+        mobile_number="09121475678",
+    )
+
+    register_user_page.verify_account_created()
+
+    register_user_page.click_continue_button()
+
+    login_user_page.verify_logged_in_user()
+
+    navbar_page.go_to_cart()
+    products_page.proceed_to_checkout()
+
+    products_page.verify_delivery()
+    products_page.verify_order()
+    products_page.message_place_order()
+
+    products_page.payment(
+        name_on_card="junuel dizon",
+        card_number="234234",
+        cvc="344",
+        expiry_month="July 10",
+        expiry_year="2028",
+    )
+
+    register_user_page.delete_account()
+
+    register_user_page.verify_account_deleted()
+
+    register_user_page.click_continue_button()
