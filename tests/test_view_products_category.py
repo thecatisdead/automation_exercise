4ff3a5af3000f74f1d7bb566d3177e7d8033abe5
a_c <peacemakerallones@gmail.com>
2026-09-14 07:39:00 +0800

    update the view category and change the POM

--- tests/test_view_products_category.py
+++ tests/test_view_products_category.py
@@ -1,24 +1,28 @@
 import pytest
 from playwright.sync_api import Page
 from pages.navbar_page import NavbarPage
-from pages.products_page import AddProductsPage
+from pages.add_products_page import AddProductsPage
 from pages.register_user_page import RegistrationPage
+from pages.category_page import CategoryPage
+
+# =========================================================================
+# TEST CASE 18: VIEW CATEGORY PRODUCTS
+# =========================================================================
 
 
-def test_view_cart_brand_products(page: Page, setup_browser):
+def test_view_products_category(page: Page, setup_browser):
     setup_browser
 
-    navbar_page = NavbarPage(page)
-    products_page = AddProductsPage(page)
-    register_user_page = RegistrationPage(page)
+    NavbarPage(page)
+    category_page = CategoryPage(page)
 
-    products_page.verify_categories_visible()
-    products_page.open_women_category()
-    products_page.verify_women_category_expanded()
-    products_page.category_dress()
-    products_page.verify_women_dress_heading()
+    category_page.verify_categories_visible()
+    category_page.open_women_category()
+    category_page.verify_women_category_expanded()
+    category_page.category_dress()
+    category_page.verify_women_dress_heading()
 
-    products_page.open_men_category()
-    products_page.verify_men_category_expanded()
-    products_page.category_tshirts()
-    products_page.verify_men_tshirts_heading()
+    category_page.open_men_category()
+    category_page.verify_men_category_expanded()
+    category_page.category_tshirts()
+    category_page.verify_men_tshirts_heading()
