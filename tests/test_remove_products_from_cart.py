1af783b4b5f18996c88a06a50942b1315377417a
a_c <peacemakerallones@gmail.com>
2026-09-14 01:07:24 +0800

    update and fix the test case remove products

--- tests/test_remove_products_from_cart.py
+++ tests/test_remove_products_from_cart.py
@@ -1,18 +1,19 @@
 import pytest
 from playwright.sync_api import Page
 from pages.navbar_page import NavbarPage
-from pages.products_page import AddProductsPage
-from pages.register_user_page import RegistrationPage
+from pages.add_products_page import AddProductsPage
+from pages.cart_page import CartPage
 
 
 def test_remove_products_from_cart(page: Page, setup_browser):
     setup_browser
 
-    navbar_page = NavbarPage(page)
-    products_page = AddProductsPage(page)
+    NavbarPage(page)
+    add_products_page = AddProductsPage(page)
+    cart_page = CartPage(page)
 
-    products_page.add_first_product_to_cart()
-    products_page.view_cart_page()
-    products_page.verify_view_cart_page()
-    products_page.remove_product()
-    products_page.verify_product_is_removed()
+    add_products_page.add_first_product_to_cart()
+    add_products_page.view_cart_page()
+    cart_page.verify_view_cart_page()
+    cart_page.remove_product()
+    cart_page.verify_product_is_removed()
