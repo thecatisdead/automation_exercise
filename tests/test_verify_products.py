4cea4a23e017340e179b3d8f173fe77b85ba2a01
a_c <peacemakerallones@gmail.com>
2026-09-13 08:34:26 +0800

    update product verification tests

--- tests/test_verify_products.py
+++ tests/test_verify_products.py
@@ -1,8 +1,7 @@
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from pages.navbar_page import NavbarPage
-from pages.products_page import ProductsPage
-from pages.products_page import AddProductsPage
+from pages.products_page import ProductsPage, AddProductsPage
 
 # =========================================================================
 # TEST CASE 8: VERIFY ALL PRODUCTS
@@ -33,9 +32,6 @@ def test_search_product(page: Page, setup_browser):
     setup_browser
 
     navbar_page = NavbarPage(page)
-
-    products_page = ProductsPage(page)
-
     products_page = AddProductsPage(page)
 
     navbar_page.go_to_products()
@@ -44,4 +40,8 @@ def test_search_product(page: Page, setup_browser):
 
     products_page.search_product("Men Tshirt")
 
-    # expect(navbar_page.search_product_input).to_have_value("Men Tshirt")
+    products_page.search_click_button()
+
+    expect(products_page.search_product_input).to_have_value("Men Tshirt")
+
+    products_page.verify_searched_products_heading()
