c8a97a56182c74ea9d9936ced9ce8026f73e01e2
a_c <peacemakerallones@gmail.com>
2026-09-15 03:52:58 +0800

    fix spacing

--- tests/test_register_email_error.py
+++ tests/test_register_email_error.py
@@ -4,17 +4,20 @@ from pages.login_user_page import LoginPage
 from pages.register_user_page import RegistrationPage
 from pages.navbar_page import NavbarPage
 
+# =========================================================================
+# TEST CASE 5: REGISTER USER WITH EXISTING EMAIL
+# =========================================================================
+
 
 def test_register_existing_email(page: Page, setup_browser):
+    setup_browser
+
+    LoginPage(page)
+
     register_user_page = RegistrationPage(page)
-    login_user_page = setup_browser
     navbar_page = NavbarPage(page)
 
-    login_user_page = LoginPage(page)
-
-    navbar_page.go_to_login()
-
+    navbar_page.go_to_login_signup()
     register_user_page.verify_new_user_signup()
-
     register_user_page.signup("junuel", "junueljon2@gmail.com")
     register_user_page.verify_signup_error()
